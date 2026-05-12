dc027069a21914ae7af7ce638fbabe903d4752cc
IT25102533 <it25102533@my.sliit.lk>
2026-05-12 15:11:41 +0530

    Initial Commit

---
+++ src/main/java/lk/ac/sliit/drivingschool/drivingschoolsystem/DrivingschoolsystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.drivingschool.drivingschoolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  DrivingschoolsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingschoolsystemApplication.class, args);
+	}
+
+}
