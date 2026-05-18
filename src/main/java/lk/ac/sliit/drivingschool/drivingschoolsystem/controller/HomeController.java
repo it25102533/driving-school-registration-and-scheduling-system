3a79271561746bbfa5bb14a79a486f67db63059b
IT25102533 <it25102533@my.sliit.lk>
2026-05-18 19:35:19 +0530

    Created about us, home and contact us page

---
+++ src/main/java/lk/ac/sliit/drivingschool/drivingschoolsystem/controller/HomeController.java
@@ -0,0 +1,23 @@
+package lk.ac.sliit.drivingschool.drivingschoolsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+}
